ecb7d5f7b0724fb0b57ccfbc4245fa7344f38c9d
Hoon <shoh@help-me.kr>
2021-02-22 15:20:01 +0900

    add bootstrap for ui purpose

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import PostCreate from './PostCreate';
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>글 작성하기</h1>
       <PostCreate/>
     </div>
