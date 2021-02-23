e9059857c0a85ae9c305d2aac37cf47f7aaeebf1
Hoon <shoh@help-me.kr>
2021-02-23 11:26:32 +0900

    add query service to provide all comments and posts from memory

---
+++ query/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const posts = {};
+
+app.post('/events', (req, res) => {
+ const { type, data } = req.body;
+
+ if (type === 'PostCreated') {
+   const { id, title } = data;
+   
+   posts[id] = { id, title, comments: [] };
+ };
+
+ if (type === 'CommentCreated') {
+  const { id, content, postId } = data;
+  const post = posts[postId];
+  
+  post.comments.push({ id, content });
+ }
+
+ //  디버깅 목적의 콘솔
+ console.log(posts)
+ res.send({})
+})
+
+// 다른 서비스에서 포스트 데이터 요청시, 포스트와 연결된 코멘트 모두 제공
+app.get('/posts', (req, res) => {
+  res.send(posts)
+});
+
+app.listen(4002, () => {
+  console.log('query running on port 4002');
+});
