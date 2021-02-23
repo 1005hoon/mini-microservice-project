cf91b9216a75e42096b2c6fc1373bc166917105a
Hoon <shoh@help-me.kr>
2021-02-23 18:39:06 +0900

    add feature to synchronize data when certain service goes down

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,10 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+// memory data storage for all the events sent
+const events = [];
+
 // 전달받은 event를 연결된 서비스 모두에 다시 전달해줍니다
 app.post('/events', (req, res) => {
   const event = req.body;
-  
+  events.push(event)
+
   // 이벤트 전송 실패시 핸들링 기능 추가 필요함
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
@@ -16,6 +20,11 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+})
+
+// 다른 서비스가 다운되었을때, 이벤트 버스에 저장된 이벤트를 불러와 데이터 sync작업 실행
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 
@@ -7,33 +8,35 @@ app.use(cors());
 app.use(express.json());
 
 const posts = {};
-
-app.post('/events', (req, res) => {
- const { type, data } = req.body;
-
- if (type === 'PostCreated') {
-   const { id, title } = data;
-   
-   posts[id] = { id, title, comments: [] };
- };
-
- if (type === 'CommentCreated') {
-  const { id, content, postId, status } = data;
-  const post = posts[postId];
-  
-  post.comments.push({ id, content, status });
- }
-
- if (type === 'CommmentUpdated') {
+const handleEvent = ({type, data}) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    
+    posts[id] = { id, title, comments: [] };
+  };
+ 
+  if (type === 'CommentCreated') {
    const { id, content, postId, status } = data;
    const post = posts[postId];
-   const comment = post.comments.find(comment => comment.id === id);
-   comment.status = status;
-   comment.content = content;   
- }
+   
+   post.comments.push({ id, content, status });
+  }
+ 
+  if (type === 'CommmentUpdated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find(comment => comment.id === id);
+    comment.status = status;
+    comment.content = content;   
+  }
+}
 
- //  디버깅 목적의 콘솔
- res.send({})
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+
+  //  디버깅 목적의 콘솔
+  res.send({})
 })
 
 // 다른 서비스에서 포스트 데이터 요청시, 포스트와 연결된 코멘트 모두 제공
@@ -41,6 +44,13 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 });
 
-app.listen(4002, () => {
-  console.log('query running on port 4002');
+app.listen(4002, async () => {
+  console.log('synchronizing data with eventbus');
+
+  const res = await axios.get('http://localhost:4005/events')
+
+  for (let event of res.data){
+    console.log(Procoessing event)
+  }
+
 });
